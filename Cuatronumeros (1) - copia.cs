a1460d5a94b8f811b1e48bff65571dbba8b4ae0b
1314clary <147454353+1314clary@users.noreply.github.com>
2024-09-03 10:41:33 -0400

    Add files via upload

---
+++ Cuatronumeros (1) - copia.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Serie_de_numeros
+{
+
+	class Program
+	{
+
+		public static void Main()
+		{
+			Console.WriteLine("Ingrese cuatro numeros");
+
+			int a = Convert.ToInt32(Console.ReadLine());
+			int b = Convert.ToInt32(Console.ReadLine());
+			int c = Convert.ToInt32(Console.ReadLine());
+			int d = Convert.ToInt32(Console.ReadLine());
+
+			if (a == b || a == c || a == d || b == c || b == d || c == d) { Console.WriteLine("Los números no pueden ser iguales"); }
+			else if (a > b && a > c && a > d) { Console.WriteLine("El número mayor es " + a); }
+			else if (b > a && b > c && b > d) { Console.WriteLine("El número mayor es " + b); }
+			else if (c > a && c > b && c > d) { Console.WriteLine("El número mayor es " + c); }
+			else if (d > a && d > b && d > c) { Console.WriteLine("El número mayor es" + d); }
+		}
+	}
+}
